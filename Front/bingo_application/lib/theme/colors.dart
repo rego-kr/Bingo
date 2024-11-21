7d36ec48bae6aba388f1bf14b4dd90932293122d
Mirage <sh55744@gmail.com>
2024-11-21 15:45:36 +0900

    Add Color&Text Style

---
+++ Front/bingo_application/lib/theme/colors.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color gray_0 = Color(0xFFFFFFFF);
+  static const Color gray_50 = Color(0xFFF2F2F2);
+  static const Color gray_100 = Color(0xFFE6E6E6);
+  static const Color gray_200 = Color(0xFFCDCDCD);
+  static const Color gray_300 = Color(0xFFB3B3B3);
+  static const Color gray_400 = Color(0xFF9A9A9A);
+  static const Color gray_500 = Color(0xFF818181);
+  static const Color gray_600 = Color(0xFF686868);
+  static const Color gray_700 = Color(0xFF4E4E4E);
+  static const Color gray_800 = Color(0xFF353535);
+  static const Color gray_900 = Color(0xFF1C1C1C);
+
+  static const Color brand_40 = Color(0xFFFBFEF1);
+  static const Color brand_100 = Color(0xFFF2FDDD);
+  static const Color brand_300 = Color(0xFFCDF899);
+  static const Color brand_500 = Color(0xFF95E954);
+  static const Color brand_700 = Color(0xFF53A72A);
+  static const Color brand_900 = Color(0xFF246F10);
+
+  static const Color yellow_100 = Color(0xFFFFFCD9);
+  static const Color yellow_300 = Color(0xFFFFF58D);
+  static const Color yellow_500 = Color(0xFFFFEA42);
+  static const Color yellow_700 = Color(0xFFB7A321);
+  static const Color yellow_900 = Color(0xFF7A690C);
+
+  static const Color red_100 = Color(0xFFFFE9DF);
+  static const Color red_300 = Color(0xFFFFADA1);
+  static const Color red_500 = Color(0xFFFF636B);
+  static const Color red_700 = Color(0xFFB73150);
+  static const Color red_900 = Color(0xFF7A133D);
+
+  static const Color blue_100 = Color(0xFFD0F3FE);
+  static const Color blue_300 = Color(0xFF72CCFE);
+  static const Color blue_500 = Color(0xFF1691FC);
+  static const Color blue_700 = Color(0xFF0B54B5);
+  static const Color blue_900 = Color(0xFF042978);
+}
---
+++ Front/bingo_application/lib/theme/text_style.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+
+class AppTextStyle{
+  /*글자색 변경을 원하면 copyWith()사용 아래는 예시
+  * style: AppTextStyle.test.copyWith(
+  *   color: Colors.blue
+  * ),
+  * */
+
+
+  static const TextStyle largeTitleBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 34,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle title1Bold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 28,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle title2Bold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 22,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle title3Bold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle largeTitleReg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 34,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle title1Reg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 28,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle title2Reg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 22,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle title3Reg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle headlineBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle bodyBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle headlineReg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle bodyReg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 17,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle calloutBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle subheadBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 15,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle footnoteBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle caption1Bold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle caption2Bold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 11,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle calloutReg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle subheadReg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 15,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle footnoteReg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle caption1Reg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    color: AppColors.gray_900,
+  );
+
+  static const TextStyle caption2Reg = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 11,
+    color: AppColors.gray_900,
+  );
+
+}
